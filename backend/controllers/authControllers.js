163ed1b80f4f2dcb19e507914b2011c88d1791e5
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2023-12-31 20:39:17 +0100

    Add files via upload

---
+++ backend/controllers/authControllers.js
@@ -0,0 +1,97 @@
+//Controllers are the function associated with the endpoints
+
+const User = require("../models/user")
+const { hashPassword, comparePassword } = require("../helpers/auth")
+const jwt = require("jsonwebtoken")
+
+
+const test = (req, res) => {
+    res.json("test is defined")
+}
+
+
+//Register endpoint
+const registerUser = async (req, res) => {
+    try{
+        if(!password || password.length < 6){
+            return res.json({
+                error: "Password with min 6 characters required"
+            })
+        }
+
+        const { email, password } = req.body
+        //Check if email was entered
+        const exist = await User.findOne({ email })
+        if(exist){
+            return res.json({
+                error: "Email is already signed up"
+            })
+        }
+
+        const hashedPassword = await hashPassword(password)
+
+        //Create user in DB
+        const user = await User.create({
+            email, 
+            password : hashedPassword
+        })
+
+        return res.json(user)
+
+    } catch(error){
+        console.log(error)
+    }
+}
+
+
+//Login endpoint
+const loginUser = async (req, res) => {
+    try {
+        const {email, password} = req.body
+
+        //Check if user exist
+        const user = await User.findOne({email})
+        if(!user){
+            return res.json({
+                error: "No user found"
+            })
+        }
+
+        //Check if password match
+        const match = await comparePassword(password, user.password)
+        if(match){
+            jwt.sign({email: user.email, id: user._id}, process.env.VITE_JWT_SECRET, {}, (err, token) => {
+                if(err) throw err
+                res.cookie("token", token).json(user)
+            })
+        }
+        if(!match){
+            res.json({
+                error: "Passwords don't match"
+            })
+        }
+    } catch(error){
+        console.log(error)
+    }
+}
+
+const getProfile = (req, res) => {
+    const {token} = req.cookies
+    if(token){
+        jwt.verify(token, process.env.VITE_JWT_SECRET, {}, (err, user) => {
+            if(err) throw err
+            res.json(user)
+        })
+    } else {
+        res.json(null)
+    }
+}
+
+
+
+module.exports = {
+    test,
+    registerUser,
+    loginUser,
+    getProfile
+}
--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,24 @@
 const express = require("express")
+const dotenv = require("dotenv").config()
+const cors = require("cors")
+const { mongoose } = require("mongoose")
+const cookieParser = require("cookie-parser")
+
+
 const app = express()
+
+
+//connect to DB
+mongoose.connect(process.env.VITE_MONGO_URL)
+.then(() => console.log("DATABASE CONNECTED"))
+.catch((err) => console.log("ERROR CONNECTION DB", err))
+
+//middleware
+app.use(express.json())
+app.use(cookieParser())
+app.use(express.urlencoded({extended: false}))
+
+app.use("/", require("./routes/authRoutes"))
+
+const port = 8000
+app.listen(port, () => console.log(`Server is running on port ${port}`))
