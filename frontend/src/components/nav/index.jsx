c3e8c20c91339e5d6074e6ecbecaec5814682601
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-01-02 00:51:57 +0100

    Add files via upload

--- frontend/src/components/nav/index.jsx
+++ frontend/src/components/nav/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import './style.css'
+import { Link } from 'react-router-dom'
 
 export default function Nav() {
   return (
-    <div>
-      
-    </div>
+    <nav>
+      <Link to="/">HOME</Link>
+      <Link to="/login" >LOGIN</Link>
+      <Link to="/register">REGISTER</Link>
+      <Link className='floor-config' to="/configurator">FLOOR CONFIGURATOR</Link>
+    </nav>
   )
 }
