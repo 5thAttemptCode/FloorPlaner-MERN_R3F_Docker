609a453ebe0d9c5a2d762ecac47c342f03e5f97c
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-01-02 00:51:32 +0100

    Add files via upload

--- frontend/src/pages/configurator/index.jsx
+++ frontend/src/pages/configurator/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import './style.css'
 import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+
 
 export default function Configurator() {
   return (
     <Canvas>
-
+      <OrbitControls enableZoom={false} />
+      <mesh rotation={[1, 0, 3]}>
+        <boxGeometry args={[2, 2, 2]} />
+        <meshNormalMaterial />
+      </mesh>
     </Canvas>
   )
 }
