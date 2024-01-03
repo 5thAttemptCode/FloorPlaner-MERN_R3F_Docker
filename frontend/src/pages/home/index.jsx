050598ef2e51d08898b0549b24b54210a4e595ff
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-01-03 03:11:42 +0100

    Add files via upload

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import "./style.css"
+import { Link } from 'react-router-dom'
+
 
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <section>
+      <div className="container">
+        <div>
+          <h2>DIGITAL</h2>
+          <h2>PLANNING</h2>
+          <p>We created the SOLUTION for your floor build. We all been there;  chose <br />
+            a floor design for our new home, and when applied  you don't like it!<br />
+            That's why we created the digital floor planner: Chose between different <br />
+            options, play with the settings and order home the right quantity.<br />
+          </p>
+        </div>
+        <div>
+          <h1>DEFINING</h1>
+          <h1>&#169;FLOORS&nbsp;&nbsp;&nbsp;&nbsp;</h1>
+        </div>
+      </div>
+    </section>
   )
 }
