9c565f93e2fd3f14bad004981fe6f98215aa8756
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-01-03 01:27:57 +0100

    Add files via upload

--- frontend/src/pages/configurator/index.jsx
+++ frontend/src/pages/configurator/index.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css'
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { Center, Environment, OrbitControls } from '@react-three/drei'
+import { Room } from './model'
+import LightSwitch from './lightSwitch'
 
 
 export default function Configurator() {
+
+  const [ light, setLight ] = useState(false)
+  const [ env, setEnv ] = useState(true)
+  
   return (
-    <Canvas>
-      <OrbitControls enableZoom={false} />
-      <mesh rotation={[1, 0, 3]}>
-        <boxGeometry args={[2, 2, 2]} />
-        <meshNormalMaterial />
-      </mesh>
-    </Canvas>
+    <div className="canvas">
+      <Canvas>
+        <OrbitControls />
+        <Center>
+          <Room rotation-y={-Math.PI / 4} />
+        </Center>
+        {light && <LightSwitch brightness={2} color={"blue"} />}
+        {env && <Environment preset='city' />}
+      </Canvas>
+
+      <div className="dashboard">
+        <button onClick={() => { setLight(!light), setEnv(!env)} }>LIGHTSWITCH</button>
+      </div>
+    </div>
   )
 }
