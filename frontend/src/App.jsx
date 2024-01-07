6235034ed21911199177f41a0131f3902c9fd411
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-01-07 22:16:09 +0100

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Login from './pages/Login'
-import Register from './pages/Register'
+import Login from './pages/user/Login'
+import Register from './pages/user/Register'
 import Nav from './components/nav'
 import Home from './pages/home'
 import Configurator from './pages/configurator'
@@ -10,6 +10,7 @@ import axios from 'axios'
 import { Toaster } from 'react-hot-toast'
 import { UserContextProvider } from './context'
 import Checkout from './pages/checkout'
+import FloorMaterials from './pages/floorMaterials'
 
 
 axios.defaults.baseURL = "http://localhost:8000"
@@ -21,7 +22,6 @@ export default function App() {
     <UserContextProvider>
       <BrowserRouter>
         <Nav />
-        <Footer />
         <Toaster position="bottom-right" toastOption={{duration: 2000}} />
         <Routes>
           <Route path="/" element={<Home />} />
@@ -29,7 +29,9 @@ export default function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/configurator" element={<Configurator />} />
           <Route path="/checkout" element={<Checkout />} />
+          <Route path="/floor-materials" element={<FloorMaterials />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </UserContextProvider>
   )
