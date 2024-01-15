7d9701af0f79e0e0db2c26782f792d07f901a75a
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-01-15 23:12:06 +0100

    Add files via upload

--- frontend/src/pages/checkout/index.jsx
+++ frontend/src/pages/checkout/index.jsx
@@ -1,15 +1,25 @@
 import React, { useContext } from 'react'
 import { UserContext } from '../../context/userContext'
+import { useSnapshot } from 'valtio'
+import { state } from '../../componentsCanvas/floorMaterial'
 
 
 export default function Checkout() {
 
-  const { user } = useContext(UserContext)
-   
+  const { user } = useContext(UserContext);
+  const snap = useSnapshot(state);
+
   return (
     <section className='checkout'>
       <h1>CHECKOUT</h1>
       {!!user && (<h5>Hello {user.email}</h5>)}
+      <ul>
+        {snap.cart.items.map(item => (
+          <li key={item.id}>
+            {item.name + " - " + item.price}
+          </li>
+        ))}
+      </ul>
     </section>
   )
 }
