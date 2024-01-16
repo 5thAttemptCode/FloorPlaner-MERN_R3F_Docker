b29efb3d954530816ebb4f22c3523297158ed7e4
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-01-16 16:51:14 +0100

    Add files via upload

--- frontend/src/pages/checkout/index.jsx
+++ frontend/src/pages/checkout/index.jsx
@@ -1,25 +1,26 @@
 import React, { useContext } from 'react'
+import H3 from '../../components/subHeaderH3'
+import { CartContext } from '../../context/cartContext'
 import { UserContext } from '../../context/userContext'
-import { useSnapshot } from 'valtio'
-import { state } from '../../componentsCanvas/floorMaterial'
 
 
 export default function Checkout() {
-
-  const { user } = useContext(UserContext);
-  const snap = useSnapshot(state);
+ 
+  const { user } = useContext(UserContext)
+  const { cart } = useContext(CartContext)
 
   return (
     <section className='checkout'>
       <h1>CHECKOUT</h1>
-      {!!user && (<h5>Hello {user.email}</h5>)}
-      <ul>
-        {snap.cart.items.map(item => (
-          <li key={item.id}>
-            {item.name + " - " + item.price}
-          </li>
+      {!!user && (<H3 text={user.email} />)}
+      <div>
+        {cart.map((item, index) => (
+          <div key={index}>
+            <H3 text={item.name} />
+            <p>{item.quantity} x ${item.price} = ${item.quantity * item.price}</p>
+          </div>
         ))}
-      </ul>
+      </div>
     </section>
   )
 }
