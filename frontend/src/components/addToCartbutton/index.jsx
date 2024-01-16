aa5fb5ae376b52430aeba0f9ab2576e93e31f5ea
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-01-16 16:48:45 +0100

    Add files via upload

--- frontend/src/components/addToCartbutton/index.jsx
+++ frontend/src/components/addToCartbutton/index.jsx
@@ -1,27 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './style.css'
 import { ShoppingCart } from '@phosphor-icons/react'
-import { state } from '../../componentsCanvas/floorMaterial'
-import { useSnapshot } from 'valtio'
+import { CartContext } from '../../context/cartContext'
 
 
-export default function AddToCartButton({ id }) {
+export default function AddToCart({ floor, quantity }) {
 
-  const snap = useSnapshot(state)
-
-  const addToCart = () => {
-    if(snap.textures){
-      const textureToAdd = snap.textures.find(texture => texture.id === id)
-      if(textureToAdd){
-        const { id, name, price } = textureToAdd; 
-        state.cart.addToCart({id, name, price})
-      }
-    } else{
-      console.log("Error, texture not found")
-    }
-  }
-
-  return (
-    <button onClick={addToCart}>ADD TO &nbsp;<ShoppingCart /></button>
-  )
+  const { addToCart } = useContext(CartContext)
+  const item = { name: floor.name, price: floor.price, quantity }
+  
+  return <button onClick={() => addToCart(item)}>Add to <ShoppingCart /></button>
 }
