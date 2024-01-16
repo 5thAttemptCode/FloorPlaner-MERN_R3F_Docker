8cee39ad36f0524587d57881b860ff67c1dcfedd
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-01-16 16:49:25 +0100

    Add files via upload

--- frontend/src/components/shoppingEllipse/index.jsx
+++ frontend/src/components/shoppingEllipse/index.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css'
 import AddToCartButton from '../addToCartbutton'
 
 
-export default function ShoppingEllipse() {
+export default function ShoppingEllipse({ floor }) {
+
+  const [ quantity, setQuantity ] = useState(1)
   return (
     <div className='shopping-ellipse'>
-      <input type="number" min={1} defaultValue={1} />
-      <AddToCartButton />
+      <input 
+        type="number" 
+        min={1} 
+        value={quantity} 
+        onChange={e => setQuantity(e.target.value)} 
+      />
+      <AddToCartButton floor={floor} quantity={quantity} />
     </div>
   )
 }
