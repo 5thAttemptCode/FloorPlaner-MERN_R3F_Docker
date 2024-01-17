647697b0e2216655aeda54bc6e1692d65a5c733d
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-01-17 22:15:12 +0100

    Add files via upload

--- frontend/src/pages/checkout/index.jsx
+++ frontend/src/pages/checkout/index.jsx
@@ -1,26 +1,35 @@
 import React, { useContext } from 'react'
+import './style.css'
 import H3 from '../../components/subHeaderH3'
+import LinkButton from '../../components/linkButton'
 import { CartContext } from '../../context/cartContext'
 import { UserContext } from '../../context/userContext'
+import { Trash } from '@phosphor-icons/react'
+
 
 
 export default function Checkout() {
  
   const { user } = useContext(UserContext)
-  const { cart } = useContext(CartContext)
+  const { cart, totalPrice } = useContext(CartContext)
 
   return (
     <section className='checkout'>
       <h1>CHECKOUT</h1>
       {!!user && (<H3 text={user.email} />)}
-      <div>
+      <div className='checkout-container'>
         {cart.map((item, index) => (
-          <div key={index}>
+          <div className='checkout-item' key={index}>
             <H3 text={item.name} />
             <p>{item.quantity} x ${item.price} = ${item.quantity * item.price}</p>
+            <Trash size={32} /> 
           </div>
         ))}
+        <div className="checkout-total">
+          <H3 text={`Total: $${totalPrice()}`} />
+        </div>
       </div>
+      <LinkButton background="--accentColor" color="--lightColor">Pay and Buy</LinkButton>
     </section>
   )
 }
