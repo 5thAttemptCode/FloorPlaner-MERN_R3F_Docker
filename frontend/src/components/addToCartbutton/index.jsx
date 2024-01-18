52121890271a7e3de221a2c2d78f43225739edec
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-01-18 20:42:19 +0100

    Add files via upload

--- frontend/src/components/addToCartbutton/index.jsx
+++ frontend/src/components/addToCartbutton/index.jsx
@@ -7,7 +7,7 @@ import { CartContext } from '../../context/cartContext'
 export default function AddToCart({ floor, quantity }) {
 
   const { addToCart } = useContext(CartContext)
-  const item = { name: floor.name, price: floor.price, quantity }
+  const item = { image: floor.url, name: floor.name, price: floor.price, quantity }
   
   return <button onClick={() => addToCart(item)}>Add to <ShoppingCart /></button>
 }
