fd7901468ef3faa3352e96caff09bf38b1b69929
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-01-19 17:18:46 +0100

    Add files via upload

--- frontend/src/components/addToCartbutton/index.jsx
+++ frontend/src/components/addToCartbutton/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import './style.css'
 import { ShoppingCart } from '@phosphor-icons/react'
 import { CartContext } from '../../context/cartContext'
@@ -8,6 +8,15 @@ export default function AddToCart({ floor, quantity }) {
 
   const { addToCart } = useContext(CartContext)
   const item = { image: floor.url, name: floor.name, price: floor.price, quantity }
-  
-  return <button onClick={() => addToCart(item)}>Add to <ShoppingCart /></button>
+  const [ clicked, setClicked ] = useState(false)
+
+  const handleClick = () => {
+    setClicked(true)
+    addToCart(item)
+
+    setTimeout(() => {
+      setClicked(false)
+    }, 500)
+  }
+  return <button className={clicked ? "button clicked" : "button"} onClick={handleClick}>Add to <ShoppingCart className='icon' /></button>
 }
