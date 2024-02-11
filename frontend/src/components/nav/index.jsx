5afe7a1fbb9139085cabc94cffaa1ebd506f48f3
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-02-11 14:08:46 +0100

    Add files via upload

--- frontend/src/components/nav/index.jsx
+++ frontend/src/components/nav/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import './style.css'
 import { Link } from 'react-router-dom'
 import { List, ShoppingCart, UserCircle } from '@phosphor-icons/react'
@@ -15,29 +15,36 @@ export default function Nav() {
   const itemCount = cart.reduce((sum, item) => sum + item.quantity, 0)
   const userPath = user ? "/profile" : "/login"
 
+  const [menuVisible, setMenuVisible] = useState(false)
+
   return (
-    <nav>
-      <div>
-        <LinkButton to="/">FLOOR PLANER</LinkButton>
-        <LinkButton to="/floor-materials">MATERIALS</LinkButton>
-        <LinkButton to="/configurator" background="--accentColor" color="--lightColor">3D CONFIGURATOR</LinkButton>
+    <>
+      <nav>
+        <div>
+          <LinkButton to="/">FLOOR PLANER</LinkButton>
+          <LinkButton to="/floor-materials">MATERIALS</LinkButton>
+          <LinkButton to="/configurator" background="--accentColor" color="--lightColor">3D CONFIGURATOR</LinkButton>
+        </div>
+        <div className="logo">
+          F<span>P</span>
+        </div>
+        <div>
+          <Link to={userPath} className='icon-button'>
+            <UserCircle size={35} />
+          </Link>
+          <Link to="/checkout" className='icon-button shopping-cart'>
+            <ShoppingCart />
+            {itemCount > 0 && <span className='cart-counter'>{itemCount}</span>}
+          </Link>
+          <button onClick={() => setMenuVisible(!menuVisible)}  className='menu-button'>
+            <List size={32} />
+          </button>
+        </div>
+      </nav>
+      <div style={{ transform: menuVisible? 'translateX(0%)' : 'translateX(130%)'}} className="responsive-menu">
+
       </div>
-      <div className="logo">
-        F<span>P</span>
-      </div>
-      <div>
-        <Link to={userPath} className='icon-button'>
-          <UserCircle size={35} />
-        </Link>
-        <Link to="/checkout" className='icon-button shopping-cart'>
-          <ShoppingCart />
-          {itemCount > 0 && <span className='cart-counter'>{itemCount}</span>}
-        </Link>
-        <button className='menu-button'>
-          <List size={32} />
-        </button>
-      </div>
-    </nav>
+    </>
   )
 }
 
