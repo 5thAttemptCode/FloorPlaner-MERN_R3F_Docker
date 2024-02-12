2dfaa3bf093cc346e839162978370f2ea4ecc91d
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-02-12 15:56:26 +0100

    Add files via upload

---
+++ frontend/src/components/headerContainer/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+
+
+export default function HeaderContainer({ children }) {
+
+    return (
+    <div className='header-container'>
+      {children}
+    </div>
+  )
+}
