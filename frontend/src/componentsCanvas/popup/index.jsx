feba8f540aa8cadf3f7234cf49958d782dee9287
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-02-24 21:18:49 +0100

    Add files via upload

--- frontend/src/componentsCanvas/popup/index.jsx
+++ frontend/src/componentsCanvas/popup/index.jsx
@@ -1,15 +1,29 @@
-import * as React from 'react'
+import  React, { useEffect, useRef, useState} from 'react'
 import './style.css'
 import { Unstable_Popup as BasePopup } from '@mui/base/Unstable_Popup'
 import { Info } from '@phosphor-icons/react'
 
 
 export default function Popup() {
-  const [anchor, setAnchor] = React.useState(null)
+  const [anchor, setAnchor] = useState(null)
+  const ref = useRef()
 
   const handleClick = (event) => {
     setAnchor(anchor ? null : event.currentTarget)
   }
+
+  const handleClickOutside = (event) => {
+    if (ref.current && !ref.current.contains(event.target)){
+      setAnchor(null)
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside)
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside)
+    }
+  })
 
   const open = Boolean(anchor);
   const id = open ? 'simple-popper' : undefined
@@ -19,9 +33,9 @@ export default function Popup() {
       <button onClick={handleClick}>
         <Info size={32} />
       </button>
-      <BasePopup placement="top-start" id={id} open={open} anchor={anchor}>
+      <BasePopup placement="top-start" id={id} open={open} anchor={anchor} ref={ref}>
         <div className="base-info">
-          Click and rotate the 3D room-model and chose between our
+          Click and rotate the 3D room-model and choose between our
           high quality floor materials in the dashboard. Adjust the
           wall color to suit your personal situation and play around
           with the light switch for an even more realistic idea.
