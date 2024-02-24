a09ba597daf4d7493486b2294f4009d20173b06f
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-02-24 21:30:33 +0100

    Add files via upload

--- frontend/src/componentsCanvas/popup/index.jsx
+++ frontend/src/componentsCanvas/popup/index.jsx
@@ -2,6 +2,7 @@ import  React, { useEffect, useRef, useState} from 'react'
 import './style.css'
 import { Unstable_Popup as BasePopup } from '@mui/base/Unstable_Popup'
 import { Info } from '@phosphor-icons/react'
+import { Tooltip } from '@mui/material'
 
 
 export default function Popup() {
@@ -29,18 +30,20 @@ export default function Popup() {
   const id = open ? 'simple-popper' : undefined
 
   return (
-    <div className='popup'>
-      <button onClick={handleClick}>
-        <Info size={32} />
-      </button>
-      <BasePopup placement="top-start" id={id} open={open} anchor={anchor} ref={ref}>
-        <div className="base-info">
-          Click and rotate the 3D room-model and choose between our
-          high quality floor materials in the dashboard. Adjust the
-          wall color to suit your personal situation and play around
-          with the light switch for an even more realistic idea.
-        </div>
-      </BasePopup>
-    </div>
+    <Tooltip placement='top-start' title="Info">
+      <div className='popup'>
+        <button onClick={handleClick}>
+          <Info size={32} />
+        </button>
+        <BasePopup placement="top-start" id={id} open={open} anchor={anchor} ref={ref}>
+          <div className="base-info">
+            Click and rotate the 3D room-model and choose between our
+            high quality floor materials in the dashboard. Adjust the
+            wall color to suit your personal situation and play around
+            with the light switch for an even more realistic idea.
+          </div>
+        </BasePopup>
+      </div>
+    </Tooltip>
   )
 }
