b318eaa849ff46db0082d9829f4b960c68a9ba53
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-02-27 22:55:00 +0100

    Add files via upload

--- frontend/src/pages/userProfile/index.jsx
+++ frontend/src/pages/userProfile/index.jsx
@@ -1,12 +1,15 @@
 import React, { useContext } from 'react'
+import './style.css'
 import axios from 'axios'
 import { UserContext } from '../../context/userContext'
 import { toast } from 'react-hot-toast'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { OrderContext } from '../../context/orderContext'
 
 
 export default function UserProfile() {
 
+  //User login/-out
   const { user, setUser } = useContext(UserContext)
   const navigate = useNavigate()
 
@@ -20,11 +23,30 @@ export default function UserProfile() {
       console.error('Error during logout', error)
     }
   }
+
+  //Previous Order
+  const { order } = useContext(OrderContext)
   
   return (
-    <section>
+    <section className='user-profile'>
+      {!!user && (<h3>Hello {user.email}</h3>)}
       <button onClick={logout}>Logout</button>
-      {!!user && (<h6>hi {user.email}</h6>)}
+      <p>Your Last Order:</p>
+      {order ? 
+        <div className='prev-order'>
+          <ul>
+            {order.items.map(item => (
+              <li key={item.name}>
+              {item.quantity}x {item.name} - ${item.price}
+              </li>
+            ))}
+          </ul>
+          <p>Total: ${order.total()}</p>
+          <p>Placed on: {order.date.toLocaleString()}</p>
+        </div>
+      : 
+      <h3>Ohh you haven't placed an order before?  <br /> Do it <Link to="/">HERE</Link></h3>
+      }
     </section>
   )
 }
