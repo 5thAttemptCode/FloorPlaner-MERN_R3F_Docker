209b25b34b0b9ebe52d9e42300330834e0f997c1
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-02-29 15:19:04 +0100

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,8 @@
 import React, { useContext } from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { CartProvider } from './context/cartContext'
+import { FloorMaterialProvider } from './context/floorButtonContext'
+import { OrderProvider } from './context/orderContext'
 import { Toaster } from 'react-hot-toast'
 import { UserContext, UserContextProvider } from './context/userContext'
 import axios from 'axios'
@@ -11,16 +14,13 @@ import Home from './pages/home'
 import Login from './pages/user/Login'
 import Nav from './components/nav'
 import Register from './pages/user/Register'
-import { CartProvider } from './context/cartContext'
 import ScrollToTop  from './utils/scrollToTop'
-import { FloorMaterialProvider } from './context/floorButtonContext'
 import UserProfile from './pages/userProfile'
-import { OrderProvider } from './context/orderContext'
 
 
-axios.defaults.baseURL = "https://floor-planer-mern-r3-f-docker-server.vercel.app/"
-// axios.defaults.baseURL = "http://localhost:8000"
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL
 axios.defaults.withCredentials = true
+
 
 export default function App() {
   return (
