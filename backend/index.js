0b7df2de321b1270378c3ec6b1d08bfb79cba78f
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-09-25 22:54:23 +1300

    incl. stripe

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,38 @@
-const express = require('express')
-const dotenv = require('dotenv').config()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const cookieParser = require('cookie-parser')
+const express = require('express');
+const dotenv = require('dotenv').config();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 
-const app = express()
+const app = express();
 
 // Define specific CORS options
 const corsOptions = {
   origin: 'https://floor-planer.netlify.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'], 
-  credentials: true, 
-}
-  
-app.use(cors(corsOptions))
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+};
 
-//connect to DB
-mongoose.connect(process.env.VITE_MONGO_URL)
-.then(() => console.log("DATABASE CONNECTED"))
-.catch((err) => console.log("ERROR CONNECTION DB", err))
+app.use(cors(corsOptions));
 
-//middleware
-app.use(express.json())
-app.use(cookieParser())
-app.use(express.urlencoded({extended: false}))
+// Connect to DB
+mongoose.connect(process.env.VITE_MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('DATABASE CONNECTED'))
+  .catch((err) => console.log('ERROR CONNECTING TO DB', err));
 
-app.use("/", require('./routes/authRoutes'))
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
 
-const port = process.env.VITE_PORT
-app.listen(port, () => console.log(`Server is running on port ${port}`))
+// Routes
+app.use('/', require('./routes/authRoutes')); // Authentication routes
+app.use('/api/payment', require('./routes/paymentRoutes')); // Payment routes
+
+// Server Port
+const port = process.env.VITE_PORT || 5000; // Default to port 5000 if not specified
+app.listen(port, () => console.log(`Server is running on port ${port}`));
